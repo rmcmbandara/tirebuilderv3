43ca7b1adad51a884cef3131dedba5236ef14e80
manujanethwidu <manujanethwidu@gmail.com>
2021-12-06 13:37:41 +0530

    .

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,13 @@ const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 //TireBuilder
 const TireBuilderView = React.lazy(() => import('./views/tireBuilder/TireBuilderView'))
 
+const PidUpdate = React.lazy(() => import('./views/pidUpdate/PidUpdate'))
+const SpecUpdate = React.lazy(() => import('./views/specUpdate/SpecUpdate'))
 const routes = [
   { path: '/', exact: true, name: 'Home' },
+  { path: '/pidupdate', name: 'PidUpdate', component: PidUpdate },
+  { path: '/specupdate', name: 'SpecUpdate', component: SpecUpdate },
+  { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/tirebuilder', name: 'SpecUpdate', component: TireBuilderView },
 ]
 
--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import { INCREMENT_COUNT } from './actions'
 const initialState = {
   sidebarShow: false,
   count: 10,
 }
-const changeState = (state = initialState, { type, ...rest }) => {
+const reducer = (state = initialState, { type, ...rest }) => {
   switch (type) {
     case 'set':
       return { ...state, ...rest }
@@ -17,5 +18,7 @@ const changeState = (state = initialState, { type, ...rest }) => {
       return state
   }
 }
-const store = createStore(changeState)
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)))
 export default store
