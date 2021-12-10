3b4785ad4fa85ec8e5a8a2193e82ac48381f71d9
manujanethwidu <manujanethwidu@gmail.com>
2021-12-10 16:31:27 +0530

    .

--- src/components/TireDetailDesplayComp.js
+++ src/components/TireDetailDesplayComp.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Card, Row, Col } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
 
 const TireDetailDesplayComp = () => {
-  return <div>asdfasfda asdfas s asdfa asdf asdf asdf adfas asdf asf a asf</div>
+  const tireCodeDetail = useSelector((state) => state.tireCodeDetails)
+  const { swmsg, brand, rimsize, tiresizebasic, config, lugtypecap, tiretypecap, color, moldno } =
+    tireCodeDetail?.data?.data?.data[0]
+
+  return (
+    <Row>
+      <Col lg={10}>
+        <Card border="warning">
+          <Card.Title style={{ fontSize: '30px', color: 'blue' }}>
+            {tiresizebasic} {lugtypecap} {config} {rimsize} {tiretypecap} {brand} {swmsg}
+          </Card.Title>
+          <Card.Title>{config == 'APW' ? `Wheel Color- ${color}` : ''}</Card.Title>{' '}
+        </Card>
+      </Col>
+      <Col lg={2}>
+        <Card border="warning">
+          <Card.Title>Mold:-{moldno}</Card.Title>
+        </Card>
+      </Col>
+    </Row>
+  )
 }
 
 export default TireDetailDesplayComp
