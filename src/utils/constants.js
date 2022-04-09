fdd6702eeaa3b99c954dea41cfef9ab16af676aa
manujanethwidu <manujanethwidu@gmail.com>
2022-04-09 19:17:31 +0530

    .

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,6 @@ export const MOVING_AVERAGE_ARRAY_LENGTH = 5
 export const DIFFERENCE_SMALL_TIRES = 5
 export const DIFFERENCE_MIDIAM_TIRES = 5
 export const DIFFERENCE_LARGE_TIRES = 5
+
+export const STABILITY_WAITING = 3
+export const TIMER_INTERVAL = 100
