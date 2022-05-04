28e12bbb4ed2ff49c3edf151c742e120f8c30907
manujanethwidu <manujanethwidu@gmail.com>
2022-05-04 12:38:12 +0530

    .

--- src/views/pressStikerPrint/PressStickerPrintView.js
+++ src/views/pressStikerPrint/PressStickerPrintView.js
@@ -6,6 +6,9 @@ import SLTLDBConnection from 'src/apis/SLTLDBConnection'
 import { scaleReading } from 'src/redux/scale/scaleActions'
 import { getBandWgtTol } from 'src/utils/bandWgtTol'
 import BandWgtModel, { bandWgtCard } from '../../components/bandStikerPrint/BandWgtCard'
+import { PRINT_X, PRINT_Y } from 'src/utils/constants'
+import printerHost from 'src/apis/printerHost'
+import { notifyError } from 'src/utils/toastify'
 const PressStickerPrintView = () => {
   //UseStates
   const [value, setValue] = useState('')
@@ -40,20 +43,7 @@ const PressStickerPrintView = () => {
       clearInterval(timer)
     }
   }, [])
-  //Fetch Band data from DB
 
-  useEffect(() => {
-    fetchDataSN()
-  }, [])
-  //getFilterdSn List
-  const fetchDataSN = async () => {
-    try {
-      const response = await SLTLDBConnection.get(`band/getBandDetails`)
-      setBandDetails(response.data.rows)
-    } catch (err) {
-      console.error(err.message)
-    }
-  }
   //Convert to lovercase
   useEffect(() => {
     setBandLower(lower(bandDetails))
@@ -63,10 +53,30 @@ const PressStickerPrintView = () => {
     selectedBand.band && setIsOpen(true)
   }, [selectedBand])
 
-  //Functions-------------------------------------
-  const hideModal = () => {
-    setSelectedBand({})
-    setIsOpen(false)
+  const x = async () => {
+    var currentdate = new Date()
+
+    var datetime =
+      currentdate.getDay() +
+      '/' +
+      (currentdate.getMonth() + 1) +
+      `-- ` +
+      currentdate.getHours() +
+      ':' +
+      currentdate.getMinutes()
+
+    // Print the barcode
+    let zpl = `^XA^FO${PRINT_X + 60},${PRINT_Y - 1}^BY2 ^BCN,120,Y,N,S^FDS${value}L^XZ`
+    //zpl = `^XA^FO${PRINT_X + 60},${PRINT_Y - 1}^BY1 ^BCN,120,Y,N,S^FDS3.37L^XZ`
+    const updateBarCode = await printerHost.put(`/bc`, { zpl, bcprinter: 1 })
+    //Error in server
+    if (updateBarCode.data.error) {
+      return notifyError(updateBarCode.data.error + ' insert temp. barcode table')
+    }
+    //row count in inserted result is not equal to 1
+    if (updateBarCode.data.data !== 1) {
+      return notifyError('Not Inserted in temp barcode table')
+    }
   }
   //Convert to lower case
   function lower(obj) {
@@ -86,10 +96,12 @@ const PressStickerPrintView = () => {
         <hr />
         <InputGroup className="mb-3">
           <FormControl
+            className="fs-1"
             placeholder="ප්‍රෙස්ස් නොම්බරය ටයිප් කරන්න"
             type="text"
             value={value}
             onChange={(e) => setValue(e.target.value)}
+            maxLength="5"
           />
         </InputGroup>
       </Row>
