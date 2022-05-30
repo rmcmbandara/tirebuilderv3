b301e7a42e3e6f3941bf5b36f7baefd67429ba54
manujanethwidu <manujanethwidu@gmail.com>
2022-05-30 13:07:30 +0530

    .

--- src/views/tireBuilder/TireBuilderView.js
+++ src/views/tireBuilder/TireBuilderView.js
@@ -6,7 +6,6 @@ import SLTLDBConnection from '../../../src/apis/SLTLDBConnection'
 import { notifyError, notifyErrorQk, notifySuccess } from 'src/utils/toastify'
 import { getPidDetail } from '../pidUpdate/httpRequests/pidDetailforPid'
 import { connect } from 'react-redux'
-
 import { incrementCountAction } from '../../actions'
 import { propTypes } from 'react-bootstrap/esm/Image'
 import TireCodeInputComp from 'src/components/bulder/TireCodeInputComp'
